7035ef8cbc5b0cb3456b509fe856c4a167ee7647
Shaade <s.shaade77@gmail.com>
2022-02-11 17:08:37 +0100

    fixed #1, bad email would make the app crash

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import pathlib
 from flask import Flask, render_template, request, redirect, flash, url_for
 
-from app.exceptions import ClientError
 from app.data import clubs, competitions
 
 
@@ -17,21 +16,18 @@ def create_app(config=None):
     # making sure the folder exist
     # pathlib.Path(app.instance_path).mkdir(parents=True, exist_ok=True)
 
-    @app.errorhandler(ClientError)
-    def handle_exception(e):
-        # By default the "description" attribute is used for the error argument in the template.
-        # if an argument "error" is present in the template_kwargs, it overrides the default.
-        kwargs = {"error": e.description, **e.template_kwargs}
-        return render_template(e.template, **kwargs), e.code
-
     @app.route("/")
     def index():
         return render_template("login.html")
 
     @app.route("/summary", methods=["POST"])
     def summary():
-        club = [club for club in clubs if club["email"] == request.form["email"]][0]
-        return render_template("welcome.html", club=club, competitions=competitions)
+        club = clubs.get_by("email", request.form["email"])
+
+        if club:
+            return render_template("welcome.html", club=club, competitions=competitions)
+        else:
+            return render_template("login.html", error="Email not found"), 401
 
     @app.route("/confirm_booking", methods=["POST"])
     def confirm_booking():
